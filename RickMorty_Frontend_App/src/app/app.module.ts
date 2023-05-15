a23acc471afefa3af2a33e8818c53a19b554c5b5
Palmira Romia <kguinera@hotmail.com>
2023-05-15 13:23:11 +0200

    Some changes and component creations

--- RickMorty_Frontend_App/src/app/app.module.ts
+++ RickMorty_Frontend_App/src/app/app.module.ts
@@ -3,18 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavegacionComponent } from './navegacion/navegacion.component';
+import { HomeComponent } from './home/home.component';
+import { CharactersComponent } from './characters/characters.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavegacionComponent
+    HomeComponent,
+    CharactersComponent,
+    AboutComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, RouterModule.forRoot(appRoutes) ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ RickMorty_Frontend_App/src/app/characters/characters.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters',
+  templateUrl: './characters.component.html',
+  styleUrls: ['./characters.component.css']
+})
+export class CharactersComponent {
+  title:string="Characters";
+
+}
